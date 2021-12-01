7f8db07e8f0e1e4545d89e918095f54dc3cae7b3
kamal <dhimank079@gmail.com>
2021-12-01 19:13:51 +0530

    pagination start

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,15 +5,16 @@ export class NewsItem extends Component {
     render() {
         let {title,desp,imageUrl,newsurl} = this.props;
         return (
-            <div className="card" style={{width: "18rem"}}>
+            <>
+            <div className="card" style={{minWidth: "14rem",maxWidth:"28rem"}}>
                 <img src={imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{desp}</p>
-                    <a href={newsurl} className="btn btn-primary">Go somewhere</a>
+                    <a href={newsurl} className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
-           
+           </>
         )
     }
 }
